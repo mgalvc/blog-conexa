4bb2db0ea03f6d923ac2cc4ed1d9579f021f9902
mgalvc <matheusgcorreia@gmail.com>
2019-08-20 12:03:34 -0300

    change messages

--- protected/views/post/create.php
+++ protected/views/post/create.php
@@ -12,6 +12,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Create Post</h1>
+<h1>Escrever</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
--- protected/views/post/update.php
+++ protected/views/post/update.php
@@ -9,13 +9,10 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Post', 'url'=>array('index')),
-	array('label'=>'Create Post', 'url'=>array('create')),
-	array('label'=>'View Post', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Post', 'url'=>array('admin')),
+	array('label'=>'Cancelar', 'url'=>array('index'))
 );
 ?>
 
-<h1>Update Post <?php echo $model->id; ?></h1>
+<h1>Editar</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
